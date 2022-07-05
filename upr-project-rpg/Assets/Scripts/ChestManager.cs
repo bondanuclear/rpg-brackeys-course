235e5601a04d9a3281d4a4ff8bbced77f574d066
Andrii Bondarenko <lanbordik13@gmail.com>
2022-07-05 15:36:54 +0300

    opening chest reworked
    some refactoring and bugs fixing

--- upr-project-rpg/Assets/Scripts/ChestManager.cs
+++ upr-project-rpg/Assets/Scripts/ChestManager.cs
@@ -7,7 +7,7 @@ public class ChestManager : MonoBehaviour
     Transform target;
     UIManager uiManager;
     [SerializeField] float distanceToUnableChest = 5f;
-    bool isUsed = false;
+    [SerializeField] bool isUsed = false;
     [SerializeField] private float speedOfOpening;
     [SerializeField] Transform chestUpperPart;
     // Start is called before the first frame update
@@ -16,23 +16,56 @@ public class ChestManager : MonoBehaviour
        uiManager = FindObjectOfType<UIManager>();
        target = PlayerManager.instance.player.transform;
     }
-    public void OpenChestModel()
+    // public void OpenChestModel()
+    // {
+    //     Vector3 rotationVector = new Vector3(-90,0,0);
+    //     chestUpperPart.localRotation = Quaternion.Slerp(chestUpperPart.localRotation, Quaternion.Euler(rotationVector), Time.deltaTime * speedOfOpening);
+    //     // to open the chest Quaternion.Euler(-90,0,0)
+    //     // to close the chest Quaternion.Euler(0,0,0)
+    // }
+    IEnumerator RotateChestModel(Vector3 rotationVector)
     {
-        Vector3 rotationVector = new Vector3(-90,0,0);
-        chestUpperPart.localRotation = Quaternion.Slerp(chestUpperPart.localRotation, Quaternion.Euler(rotationVector), Time.deltaTime * speedOfOpening);
-        // to open the chest Quaternion.Euler(-90,0,0)
-        // to close the chest Quaternion.Euler(0,0,0)
+        Debug.Log("Starting to open the chest");
+        float timePassed = 0f;
+        //Vector3 rotationVector = new Vector3(-90, 0, 0);
+        while(timePassed < 0.9f)
+        {
+            timePassed += Time.deltaTime;
+            chestUpperPart.localRotation = Quaternion.Slerp(
+                chestUpperPart.localRotation,
+                 Quaternion.Euler(rotationVector),
+                  timePassed/speedOfOpening);
+                  //Debug.Log(timePassed);
+                 
+                  yield return new WaitForEndOfFrame();
+        }
+        
+        yield return null;
+        
+    }
+    IEnumerator ProcessOpenChest()
+    {
+        yield return RotateChestModel(new Vector3(-90, 0, 0));
+        OpenUI();
+        yield return null;
+        isUsed = true;
+    }
+    IEnumerator ProcessCloseChest()
+    {
+        yield return RotateChestModel(new Vector3(0,0,0));
     }
     public void SetUsed(bool state)
     {
-        isUsed = state;
-        OpenUI();
+        // isUsed = state;
+        //OpenUI();
+        StartCoroutine(ProcessOpenChest());
+        
     }
     void OpenUI()
     {
         uiManager.ProcessUI(true);
         uiManager.ProcessChestUI(true);
-        OpenChestModel();
+        //OpenChestModel();
     }
     void CloseUI()
     {
@@ -60,6 +93,7 @@ public class ChestManager : MonoBehaviour
 
     private void UnableChest()
     {
+        StartCoroutine(ProcessCloseChest());
         CloseUI();
         isUsed = false;
     }
